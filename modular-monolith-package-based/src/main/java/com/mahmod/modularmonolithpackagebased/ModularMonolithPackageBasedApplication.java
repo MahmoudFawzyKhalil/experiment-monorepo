71f564d0fc97930f130d20e3abc7f5edc9329775
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-08-30 01:04:09 +0200

    Create modular-monolith-package-based project

---
+++ modular-monolith-package-based/src/main/java/com/mahmod/modularmonolithpackagebased/ModularMonolithPackageBasedApplication.java
@@ -0,0 +1,13 @@
+package com.mahmod.modularmonolithpackagebased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularMonolithPackageBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModularMonolithPackageBasedApplication.class, args);
+	}
+
+}
---
+++ modular-monolith-package-based/src/test/java/com/mahmod/modularmonolithpackagebased/ModularMonolithPackageBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmod.modularmonolithpackagebased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularMonolithPackageBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
