33e5282a28a8814798dfba5c025875dd45c35714
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-09-09 21:34:14 +0200

    Create Spring Security opaque token project

---
+++ spring-security/ss-old-opaque-tokens/src/main/java/com/example/ssoldopaquetokens/SsOldOpaqueTokensApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssoldopaquetokens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsOldOpaqueTokensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsOldOpaqueTokensApplication.class, args);
+	}
+
+}
