f09d6a80600f68f59485b0c2e46902fe4a0b3444
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-09-10 12:27:06 +0200

    Test customizing http security using configure method

---
+++ spring-security/spring-security-in-action-ch-02/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+//        http.httpBasic();
+
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .permitAll();
+    }
+}
