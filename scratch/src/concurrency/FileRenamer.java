6d568f8c7d0254606f2decd33171db422168003b
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-10-07 20:39:48 +0200

    Fix bugs

--- scratch/src/concurrency/FileRenamer.java
+++ scratch/src/concurrency/FileRenamer.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class FileRenamer {
     public static void main(String[] args) throws IOException {
-        Path directory = Path.of("/home/mahmoud/Downloads/Heinz Kabutz - Data Structures in Java");
+        Path directory = Path.of("path/to/file");
 
         Path namesPath = directory.resolve("names.txt");
 
@@ -18,13 +18,19 @@ public class FileRenamer {
 
         List<Path> files = Files.list(directory)
                 .filter(path -> path.toString().endsWith(".mp4"))
+                .sorted((p1, p2) -> {
+                    int p1Num = Integer.parseInt(p1.getFileName().toString().replace("lesson", "").replace(".mp4", ""));
+                    int p2Num = Integer.parseInt(p2.getFileName().toString().replace("lesson", "").replace(".mp4", ""));
+                    return Integer.compare(p1Num, p2Num);
+                })
+                .peek(System.out::println)
                 .toList();
 
         for (int i = 0; i < files.size(); i++) {
             Path file = files.get(i);
             String newName = names.get(i);
 
-            String x = i <= 8 ? "0" + (i + 1) + " - " + newName + ".mp4" : (i + 1) + " - " + newName + ".mp4";
+            String x = i <= 8 ? "0" + (i + 27) + " - " + newName + ".mp4" : (i + 27) + " - " + newName + ".mp4";
 
             Path newFile = file.resolveSibling(Path.of(x));
 
